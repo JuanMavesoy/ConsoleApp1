bd9bac558599d7142d9beaa7fe22a3423223fc45
Juan Carlos Mavesoy Orozco <jua.mavesoy@udla.edu.co>
2023-07-19 16:19:49 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//HttpClient client = new HttpClient();
+
+// HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+
+using ConsoleApp1;
+using System.Net.Http.Json;
+using System.Net.Security;
+using System.Text.Json.Nodes;
+
+
+try
+{
+    string url = "https://raw.githubusercontent.com/Serempre/test-json/main/list1.json";
+
+    using var HttpClient = new HttpClient();
+
+    List<User> result = await HttpClient.GetFromJsonAsync<List<User>>(url);
+
+    var orderResult = result.OrderBy(x=> x.kpi.Speed).ToList();
+
+    foreach (User user in orderResult)
+    {
+        Console.WriteLine($"{user.Id}");
+        Console.WriteLine($"{user.Name}");
+        Console.WriteLine($" KPI: Speed {user.kpi.Speed} | Level {user.kpi.Level}");
+    }
+
+}
+catch (Exception)
+{
+
+    throw;
+}
+
